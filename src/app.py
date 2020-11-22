081af7d9b5ad5c8b557883027580ef0e390839d9
kzy611 <mnkckzy@gmail.com>
2020-11-23 00:40:24 +0900

    appモデル追加

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from utils import select_method
+from data import Data
+from model import Model
+
+class App:
+    def __init__(self, file_info):
+        data = Data(file_info)
+        model = Model(file_info)
+        # 選択肢
+        selection = {
+            '1': {
+                'name': 'データの確認・追加・削除など',
+                'method': data.select_method,
+            },
+            '2': {
+                'name': '学習、予測',
+                'method': model.select_method,
+            },
+        }
+        # 選ぶ
+        select_method(selection)
+
+if __name__ == '__main__':
+    import config
+    app = App(config.file_info)
--- src/core.py
+++ src/core.py
@@ -1,27 +1,4 @@
-from utils import select_method
-from data import Data
-from model import Model
+import config
+from app import App
 
-file_info = {
-        'paths': {
-                'data':  '../data/',
-                'model': '../models/',
-        },
-        'files': {
-                'dataframe':  'event_data.csv',
-        },
-}
-
-data = Data(file_info)
-model = Model(file_info)
-
-modes = {
-        '1': {'name': 'データの確認・追加・削除など',
-              'method': data.select_method,
-              },
-        '2': {'name': '学習、予測',
-              'method': model.select_method,
-              },
-        }
-
-select_method(modes)
+app = App(config.file_info)
