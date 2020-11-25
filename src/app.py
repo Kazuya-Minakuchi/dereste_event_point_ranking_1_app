16722cd808c1074d527efc9714a07e2ae52195f6
kzy611 <mnkckzy@gmail.com>
2020-11-25 09:20:49 +0900

    app内の選択肢表示をメソッド化

--- src/app.py
+++ src/app.py
@@ -4,17 +4,19 @@ from src.model import Model
 
 class App:
     def __init__(self, file_info):
-        data = Data(file_info)
-        model = Model(file_info)
-        # 選択肢
+        self.data = Data(file_info)
+        self.model = Model(file_info)
+    
+    # 選択肢
+    def select_method(self):
         selection = {
             '1': {
                 'name': 'データの確認・追加・削除など',
-                'method': data.select_method,
+                'method': self.data.select_method,
             },
             '2': {
                 'name': '学習、予測',
-                'method': model.select_method,
+                'method': self.model.select_method,
             },
         }
         # 選ぶ
--- src/core.py
+++ src/core.py
@@ -3,6 +3,7 @@ from src.app import App
 
 def main():
     app = App(config.file_info)
+    app.select_method()
 
 if __name__ == '__main__':
     main()
