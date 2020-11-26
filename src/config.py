bcd27cfc2a5b33a863d7bfa63a1bea3918914ebb
kzy611 <mnkckzy@gmail.com>
2020-11-26 22:48:09 +0900

    パスを絶対パスで指定するよう変更

--- src/config.py
+++ src/config.py
@@ -1,8 +1,11 @@
+import os
+
+cur_path = os.path.dirname(__file__)
 
 file_info = {
         'paths': {
-                'data':  '../data/',
-                'model': '../models/',
+                'data':  os.path.join(cur_path, '../data/'),
+                'model': os.path.join(cur_path, '../models/'),
         },
         'files': {
                 'dataframe':  'event_data.csv',
