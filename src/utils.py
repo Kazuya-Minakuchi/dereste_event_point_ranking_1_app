3ad6df288f62ce755ecf2c44d5f57e2d3604bd71
kzy611 <mnkckzy@gmail.com>
2020-11-27 10:55:17 +0900

    Selectionsクラスを作ったので、不要になった関数削除

--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,4 @@
 import pickle
-from inputs import input_data
-
-# メソッドの選択に使う
-def select_method(selections):
-    """
-    selectionsの中身
-    key:    数字(文字列)
-        name:   表示する文字
-        method: 実行するメソッド
-    """
-    while True:
-        # 選択肢表示
-        print('モードを選んでください')
-        for key, value in selections.items():
-            print(key, ':', value['name'])
-        # 選択
-        mode_num = input_data('数字')
-        # Noneが帰ってきたらキャンセル
-        if mode_num is None:
-            return
-        print(selections[mode_num]['name'])
-        # 正しい値が入力されていたら、実行
-        try:
-            selections[mode_num]['method']()
-        # 値が間違っていたら、やり直し
-        except KeyError:
-            print('正しい数字を入力してください')
-        print('')
 
 # 辞書データの関数を実行し、戻り値の入った辞書を返す
 def execute_dict_functions(in_dict):
